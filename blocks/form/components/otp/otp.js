3f92e7eb8b3e264ccc1dd0575164ed1ebff8fea5
Yash-099 <ybhavsar@adobe.com>
2025-01-14 12:40:51 +0530

    fix: otp component

--- blocks/form/components/otp/otp.js
+++ blocks/form/components/otp/otp.js
@@ -1,26 +1,23 @@
-export default function decorate(fieldDiv, fieldJson) {
-  console.log('OTP DIV: ', fieldDiv);
-  console.log('OTP JSON: ', fieldJson);
+export default function decorate(fieldDiv) {
+  const input = fieldDiv.querySelector('input');
+  input.type = 'password';
+  input.classList.add('otp-input');
 
-  const inputWrapper = document.createElement('div');
-  inputWrapper.className = 'otp-input-wrapper';
+  const toggleButton = document.createElement('button');
+  toggleButton.type = 'button';
+  toggleButton.innerText = 'Show';
+  toggleButton.classList.add('otp-toggle');
+  fieldDiv.appendChild(toggleButton);
 
-  for (let i = 0; i < 6; i += 1) {
-    const input = document.createElement('input');
-    input.type = 'number';
-    input.maxLength = 1;
-    input.className = 'otp-input';
-    input.addEventListener('input', (e) => {
-      if (e.target.value.length === 1) {
-        const nextInput = e.target.nextElementSibling;
-        if (nextInput) {
-          nextInput.focus();
-        }
-      }
-    });
-    inputWrapper.appendChild(input);
-  }
+  toggleButton.addEventListener('click', () => {
+    if (input.type === 'password') {
+      input.type = 'text';
+      toggleButton.innerText = 'Hide';
+    } else {
+      input.type = 'password';
+      toggleButton.innerText = 'Show';
+    }
+  });
 
-  fieldDiv.appendChild(inputWrapper);
   return fieldDiv;
 }
