00ef26ff1999ef8f2c14e0a69e9f4fc02ffd9665
Yash-099 <ybhavsar@adobe.com>
2025-01-21 15:59:45 +0530

    fix: carousel component

--- blocks/form/mappings.js
+++ blocks/form/mappings.js
@@ -1,6 +1,6 @@
 import { loadCSS } from '../../scripts/aem.js';
 
-let customComponents = ['range', 'scribble-signature', 'otp', 'nps-rating', 'rating-scale', 'creditcard-carousel'];
+let customComponents = ['range', 'scribble-signature', 'otp', 'nps-rating', 'rating-scale', 'card-carousel'];
 const OOTBComponentDecorators = ['file-input', 'wizard', 'modal', 'tnc', 'toggleable-link', 'rating', 'datetime', 'list', 'location'];
 
 export function setCustomComponents(components) {
