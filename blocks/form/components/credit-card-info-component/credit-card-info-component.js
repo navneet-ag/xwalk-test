8aeb945fb32d62a9c78de95a69577ec1618b5e25
Yash-099 <ybhavsar@adobe.com>
2025-01-23 16:43:03 +0530

    fix: card component

--- blocks/form/components/credit-card-info-component/credit-card-info-component.js
+++ blocks/form/components/credit-card-info-component/credit-card-info-component.js
@@ -1,27 +1,27 @@
 export default async function decorate(fieldDiv, fieldJson) {
-  const image = document.createElement('img');
-  image.src = fieldJson.image.value;
-  fieldDiv.appendChild(image);
+  // const image = document.createElement('img');
+  // image.src = fieldJson.image.value;
+  // fieldDiv.appendChild(image);
 
-  const title = document.createElement('div');
-  title.className = 'title';
-  title.textContent = fieldJson.title.value;
-  fieldDiv.appendChild(title);
+  // const title = document.createElement('div');
+  // title.className = 'title';
+  // title.textContent = fieldJson.title.value;
+  // fieldDiv.appendChild(title);
 
-  const description = document.createElement('div');
-  description.className = 'description';
-  description.textContent = fieldJson.description.value;
-  fieldDiv.appendChild(description);
+  // const description = document.createElement('div');
+  // description.className = 'description';
+  // description.textContent = fieldJson.description.value;
+  // fieldDiv.appendChild(description);
 
   const separator = document.createElement('div');
   separator.className = 'separator';
   fieldDiv.appendChild(separator);
 
-  const link = document.createElement('a');
-  link.className = 'link';
-  link.href = fieldJson.link.link;
-  link.textContent = fieldJson.link.value;
-  fieldDiv.appendChild(link);
+  // const link = document.createElement('a');
+  // link.className = 'link';
+  // link.href = fieldJson.link.link;
+  // link.textContent = fieldJson.link.value;
+  // fieldDiv.appendChild(link);
 
   return fieldDiv;
 }
