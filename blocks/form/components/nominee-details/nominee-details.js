b988e7667973ddd8f6a0552efb1db5e30c5bd61f
Yash-099 <ybhavsar@adobe.com>
2025-02-04 11:48:19 +0530

    fix: nominee details component

--- blocks/form/components/nominee-details/nominee-details.js
+++ blocks/form/components/nominee-details/nominee-details.js
@@ -1,4 +1,4 @@
-class NomineeDetails {
+class NomineeDetailsPanel {
   constructor(fieldDiv, fieldJson) {
     this.fieldDiv = fieldDiv;
     this.fieldJson = fieldJson;
@@ -8,70 +8,36 @@ class NomineeDetails {
   decorate() {
     this.fieldDiv.classList.add('nominee-details-panel');
 
-    const header = this.fieldDiv.querySelector('legend');
-    if (header) {
-      header.classList.add('nominee-details-panel__header');
-    }
+    const title = this.fieldDiv.querySelector('legend');
+    title.classList.add('nominee-details-panel__title');
+    title.innerText = 'Nominee Details';
 
-    const plainTextWrapper = this.fieldDiv.querySelector('.plain-text-wrapper');
-    if (plainTextWrapper) {
-      plainTextWrapper.classList.add('nominee-details-panel__plain-text');
-      const p = plainTextWrapper.querySelector('p');
-      if (p) {
-        p.innerText = 'Do you want to add a Nominee?';
-      }
-      const description = document.createElement('p');
-      description.classList.add('nominee-details-panel__plain-text-description');
-      description.innerText = 'You can add up to 4 nominees';
-      plainTextWrapper.appendChild(description);
-    }
+    const plainText = this.fieldDiv.querySelector('.plain-text-wrapper p');
+    plainText.classList.add('nominee-details-panel__plain-text');
+    plainText.innerText = 'Do you want to add a Nominee? You can add up to 4 nominees';
 
-    const radioGroupWrapper = this.fieldDiv.querySelector('.radio-group-wrapper');
-    if (radioGroupWrapper) {
-      radioGroupWrapper.classList.add('nominee-details-panel__radio-group');
-      const radioButtons = radioGroupWrapper.querySelectorAll('input[type="radio"]');
-      radioButtons.forEach((radio, index) => {
-        if (index === 0) {
-          radio.checked = true;
-        }
-      });
-    }
+    const radioGroup = this.fieldDiv.querySelector('.radio-group-wrapper');
+    radioGroup.classList.add('nominee-details-panel__radio-group');
 
-    const innerPanel = this.fieldDiv.querySelector('.panel-wrapper');
-    if (innerPanel) {
-      innerPanel.classList.add('nominee-details-panel__inner-panel');
-    }
+    const subPanel = this.fieldDiv.querySelector('.panel-wrapper:nth-of-type(2)');
+    subPanel.classList.add('nominee-details-panel__sub-panel');
 
-    const checkboxWrapper = this.fieldDiv.querySelector('.checkbox-wrapper');
-    if (checkboxWrapper) {
-      checkboxWrapper.classList.add('nominee-details-panel__checkbox');
-      const label = checkboxWrapper.querySelector('label');
-      if (label) {
-        label.innerText = this.fieldJson.nominee;
-      }
-    }
+    const checkboxWrapper = subPanel.querySelector('.checkbox-wrapper');
+    checkboxWrapper.classList.add('nominee-details-panel__checkbox');
+    const checkboxLabel = checkboxWrapper.querySelector('label');
+    checkboxLabel.innerText = this.fieldJson.nominee;
 
-    const editButtonWrapper = this.fieldDiv.querySelector('.button-wrapper[data-active="true"]');
-    if (editButtonWrapper) {
-      const editButton = editButtonWrapper.querySelector('button');
-      if (editButton) {
-        editButton.classList.add('nominee-details-panel__edit-button');
-        editButton.innerText = 'Edit';
-      }
-    }
+    const editButtonWrapper = subPanel.querySelector('.button-wrapper');
+    const editButton = editButtonWrapper.querySelector('button');
+    editButton.classList.add('nominee-details-panel__edit-button');
 
-    const addButtonWrapper = this.fieldDiv.querySelector('.button-wrapper[data-active="true"]:last-child');
-    if (addButtonWrapper) {
-      const addButton = addButtonWrapper.querySelector('button');
-      if (addButton) {
-        addButton.classList.add('nominee-details-panel__add-button');
-        addButton.innerText = 'Add New Nominee';
-      }
-    }
+    const addButtonWrapper = this.fieldDiv.querySelector('.button-wrapper:nth-of-type(2)');
+    const addButton = addButtonWrapper.querySelector('button');
+    addButton.classList.add('nominee-details-panel__add-button');
   }
 }
 
 export default async function decorate(fieldDiv, fieldJson) {
-  const nomineeDetails = new NomineeDetails(fieldDiv, fieldJson);
-  return nomineeDetails.fieldDiv;
+  const nomineeDetailsPanel = new NomineeDetailsPanel(fieldDiv, fieldJson);
+  return nomineeDetailsPanel.fieldDiv;
 }
